fb6d8086537c2134f66dadc213d6ae208db39676
LineCatOvO <linecatstudio@outlook.com>
2026-05-30 05:30:21 +0800

    fix(test): add missing @RunWith annotation to ImageTest

--- app/src/test/java/com/droidvisor/docker/model/ImageTest.kt
+++ app/src/test/java/com/droidvisor/docker/model/ImageTest.kt
@@ -6,7 +6,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class ImageTest {
 
     private val json = Json { ignoreUnknownKeys = true; prettyPrint = true }
